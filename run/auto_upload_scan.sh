50f6966ad4f4a8d9bf1b04e3f993539d12815538
ZachGeo <zachageo95@gmail.com>
2020-04-22 21:58:24 +0300

    Fix Paths

--- run/auto_upload_scan.sh
+++ run/auto_upload_scan.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Calculate the md5 hash value of each sample and create a txt file whcich contains the md5 and the path of each sample.
-find ~/Desktop/automation/APKs -type f -exec md5sum {} + | sort -k 2 > ~/Desktop/automation/scans_comparison/samples.txt
+find ~/Automation-MobSF/APKs -type f -exec md5sum {} + | sort -k 2 > ~/Automation-MobSF/scans_comparison/samples.txt
 
 # Create a txt file which contains all the samples (md5 hash value) which have been already scanned by MobSF.
-ls ~/mobsf/Mobile-Security-Framework-MobSF/uploads/ > ~/Desktop/automation/scans_comparison/scanned_samples.txt
+ls ~/Mobile-Security-Framework-MobSF/uploads/ > ~/Automation-MobSF/scans_comparison/scanned_samples.txt
 
 # Number of samples that I want to scan.
 num_samples=`find ~/Desktop/automation/APKs/ -type f | wc -l`
@@ -13,12 +13,12 @@ num_samples=`find ~/Desktop/automation/APKs/ -type f | wc -l`
 for ((num=1; num<= "$num_samples"; num++));
 do
   # From the txt file, I store the values of md5 and path of the sample.
-  sample_md5=`sed -n -e "$num"p ~/Desktop/automation/scans_comparison/samples.txt | awk '{print $1}'`
-  sample_path=`sed -n -e "$num"p ~/Desktop/automation/scans_comparison/samples.txt | awk '{print $2}'`
+  sample_md5=`sed -n -e "$num"p ~/Automation-MobSF/scans_comparison/samples.txt | awk '{print $1}'`
+  sample_path=`sed -n -e "$num"p ~/Automation-MobSF/scans_comparison/samples.txt | awk '{print $2}'`
 
   # Check if the sample has already been scanned.
   # If "YES", remove file from those which I want to be scanned. If "NO", continue with scanning.  
-  if grep -q "${sample_md5}" ~/Desktop/automation/scans_comparison/scanned_samples.txt;
+  if grep -q "${sample_md5}" ~/Automation-MobSF/scans_comparison/scanned_samples.txt;
   then
    rm "$sample_path"
 
@@ -42,10 +42,10 @@ do
 
    # Run script to start Dynamic Analysis
    sleep 2m
-   package_sample=$(python3 ~/Desktop/automation/run/dynamic_scan.py "$name_sample" 2>&1)
+   package_sample=$(python3 ~/Automation-MobSF/run/dynamic_scan.py "$name_sample" 2>&1)
 
    # Run script to download Static and Dynamic Report.
    sleep 10m
-   python3 ~/Desktop/automation/run/scan_to_pdf.py "$sample_md5" "$package_sample"
+   python3 ~/Automation-MobSF/run/scan_to_pdf.py "$sample_md5" "$package_sample"
   fi
 done
--- run/scan_to_pdf.py
+++ run/scan_to_pdf.py
@@ -12,10 +12,10 @@ md5_sample = str(sys.argv[1])
 package_sample = str(sys.argv[2])
 
 # Create Report directory of this sample.
-os.system(f'mkdir ~/Desktop/automation/reports/{md5_sample}')
+os.system(f'mkdir ~/Automation-MobSF/reports/{md5_sample}')
 
 # STATIC REPORT
 os.system(f'curl -X POST --url http://localhost:8000/api/v1/download_pdf --output ~/Desktop/automation/reports/{md5_sample}/Static --data "hash=md5_sample" -H "Authorization:7749e3e9e6db8c3fc2f890348d81f084cc3ad8a5abac4592f6ceea5659b12caf"')
 
 # DYNAMIC REPORT
-pdfkit.from_url(f'http://localhost:8000/dynamic_report/?hash={md5_sample}&package={package_sample}', f'~/Desktop/automation/reports/{md5_sample}/Dynamic.pdf')
+pdfkit.from_url(f'http://localhost:8000/dynamic_report/?hash={md5_sample}&package={package_sample}', f'~/Automation-MobSF/reports/{md5_sample}/Dynamic.pdf')
