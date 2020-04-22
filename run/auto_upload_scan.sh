066aac06373296bb79f82c9890e52e9ba0cf08dc
ZachGeo <zachageo95@gmail.com>
2020-04-22 15:34:02 +0300

    Scan Multiple Files.

--- run/auto_upload_scan.sh
+++ run/auto_upload_scan.sh
@@ -1,46 +1,51 @@
-
 #!/bin/bash
 
-# Find into the APKs file, only the files (not dir) and calculate the md5 hash for each file and pipe it sorted by hash value to a new file.
-find /home/zach/Desktop/automation/APKs -type f -exec md5sum {} + | sort -k 2 > /home/zach/Desktop/automation/scans_comparison/upload_dir.txt
+# Calculate the md5 hash value of each sample and create a txt file whcich contains the md5 and the path of each sample.
+find ~/Desktop/automation/APKs -type f -exec md5sum {} + | sort -k 2 > ~/Desktop/automation/scans_comparison/samples.txt
 
-# Insert text into the first line of the selected file.
-sed -i '1i #MD5 FilePath' /home/zach/Desktop/automation/scans_comparison/upload_dir.txt
+# Create a txt file which contains all the samples (md5 hash value) which have been already scanned by MobSF.
+ls ~/mobsf/Mobile-Security-Framework-MobSF/uploads/ > ~/Desktop/automation/scans_comparison/scanned_samples.txt
 
-# File which has the name of the dirs of the samples which are already sanned.
-ls /home/zach/mobsf/Mobile-Security-Framework-MobSF/uploads/ > /home/zach/Desktop/automation/scans_comparison/mobsf_uploaded.txt
+# Number of samples that I want to scan.
+num_samples=`find ~/Desktop/automation/APKs/ -type f | wc -l`
 
-# Loop over all the lines of the file, doing something.
-while IFS= read -r line; do
- # Read the first 32 number of each line of the file and assign to a variable. 
- sample_md5="$(head -c 32)"
- # Read the path of the APK, which we want to scan.
- sample_path="$(cut -d ' ' -f 3-)"
+# Loop over the number of samples. 
+for ((num=1; num<= "$num_samples"; num++));
+do
+  # From the txt file, I store the values of md5 and path of the sample.
+  sample_md5=`sed -n -e "$num"p ~/Desktop/automation/scans_comparison/samples.txt | awk '{print $1}'`
+  sample_path=`sed -n -e "$num"p ~/Desktop/automation/scans_comparison/samples.txt | awk '{print $2}'`
 
- if grep -q "${sample_md5}" /home/zach/Desktop/automation/scans_comparison/mobsf_uploaded.txt;
- then
-  rm "$sample_path"
-  #curl -X POST --url http://localhost:8000/api/v1/delete_scan --data "hash=$sample_md5" -H "Authorization:7749e3e9e6db8c3fc2f890348d81f084cc3ad8a5abac4592f6ceea5659b12caf" ;
- else
-  test_sample="${sample_path}"
-  name_sample="${sample_path:24}"
-  type_sample="${sample_path: -3}"
+  # Check if the sample has already been scanned.
+  # If "YES", remove file from those which I want to be scanned. If "NO", continue with scanning.  
+  if grep -q "${sample_md5}" ~/Desktop/automation/scans_comparison/scanned_samples.txt;
+  then
+   rm "$sample_path"
 
-  if [ "$type_sample" == "ppx" ]; then type_sample="appx"; fi
+  else
+   test_sample="${sample_path}"
+   name_sample="${sample_path:35}"
+   type_sample="${sample_path: -3}"
 
-  curl -F "file=@$test_sample" http://localhost:8000/api/v1/upload -H "Authorization:7749e3e9e6db8c3fc2f890348d81f084cc3ad8a5abac4592f6ceea5659b12caf";
-  sleep 1m
-  curl -X POST --url http://localhost:8000/api/v1/scan --data "scan_type=$type_sample&file_name=$name_sample&hash=$sample_md5" -H "Authorization:7749e3e9e6db8c3fc2f890348d81f084cc3ad8a5abac4592f6ceea5659b12caf"
-  #sleep 2m
-  #curl -X POST --url http://localhost:8000/api/v1/download_pdf --output StaticReport --data "hash=$sample_md5" -H "Authorization:7749e3e9e6db8c3fc2f890348d81f084cc3ad8a5abac4592f6ceea5659b12caf"
+   if [ "$type_sample" == "ppx" ]; then type_sample="appx"; fi
 
-  sleep 2m
-  python3 /home/zach/Desktop/automation/run/static_score.py "$sample_md5"
+   # Upload sample.
+   curl -F "file=@$test_sample" http://localhost:8000/api/v1/upload -H "Authorization: 7749e3e9e6db8c3fc2f890348d81f084cc3ad8a5abac4592f6ceea5659b12caf"
 
-  sleep 2m
-  package_sample=$(python3 /home/zach/Desktop/automation/run/dynamic_scan.py "$name_sample" 2>&1)
+   # Scan sample. Static Analysis
+   sleep 1m
+   curl -X POST --url http://localhost:8000/api/v1/scan --data "scan_type=$type_sample&file_name=$name_sample&hash=$sample_md5" -H "Authorization: 7749e3e9e6db8c3fc2f890348d81f084cc3ad8a5abac4592f6ceea5659b12caf"
 
-  sleep 10m
-  python3 /home/zach/Desktop/automation/run/scan_to_pdf "$sample_md5" "$package_sample"
- fi
-done < /home/zach/Desktop/automation/scans_comparison/upload_dir.txt
+   # Run scrit to check the Security Static Score of the sample.
+   sleep 2m
+   python3 ~/Desktop/automation/run/static_score.py "$sample_md5"
+
+   # Run script to start Dynamic Analysis
+   sleep 2m
+   package_sample=$(python3 ~/Desktop/automation/run/dynamic_scan.py "$name_sample" 2>&1)
+
+   # Run script to download Static and Dynamic Report.
+   sleep 10m
+   python3 ~/Desktop/automation/run/scan_to_pdf.py "$sample_md5" "$package_sample"
+  fi
+done
