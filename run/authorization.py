8c31fa0bd2977028edabd6114884f01623c84316
ZachGeo <zachageo95@gmail.com>
2020-04-23 16:07:43 +0300

    Authorization API Key

---
+++ run/authorization.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+''' Extract API Authorization Key '''
+
+from bs4 import BeautifulSoup as BS
+import requests
+import os
+
+url="http://localhost:8000/api_docs"
+
+page = requests.get(url)
+soup = BS(page.content, 'html.parser')
+
+rest_api_key = soup.find("p", {"class":"lead"})
+value = rest_api_key.find('code').getText()
+
+path = os.path.join(os.path.expanduser('~'), 'Automation-MobSF', 'authorization_api_key.txt')
+
+f=open(path,"w+")
+f.write(value)
+f.close()
