d509285e310412fcb8a7afd339f48ae6848c084a
ZachGeo <zachageo95@gmail.com>
2020-04-23 20:23:25 +0300

    Add Comments

--- run/authorization.py
+++ run/authorization.py
@@ -6,6 +6,10 @@ from bs4 import BeautifulSoup as BS
 import requests
 import os
 
+''' 
+Navigate through the URL and find the class 
+which contains the Authorization API Key. 
+'''
 url="http://localhost:8000/api_docs"
 
 page = requests.get(url)
@@ -14,6 +18,10 @@ soup = BS(page.content, 'html.parser')
 rest_api_key = soup.find("p", {"class":"lead"})
 value = rest_api_key.find('code').getText()
 
+''' 
+Create a text file which contains the API key, 
+into the Automation-MobSF folder.
+'''
 path = os.path.join(os.path.expanduser('~'), 'Automation-MobSF', 'authorization_api_key.txt')
 
 f=open(path,"w+")
