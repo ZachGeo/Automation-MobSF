4db4786ac93f125a942668846abecb4495f6e19d
ZachGeo <zachageo95@gmail.com>
2020-05-05 18:26:21 +0300

    Fix Errors and Issues of storing the Reports

--- run/reports.py
+++ run/reports.py
@@ -15,10 +15,10 @@ authorization_api_key = str(sys.argv[3])
 os.system(f'mkdir ~/Automation-MobSF/reports/{md5_sample}')
 
 # STATIC REPORT - PDF
-os.system(f'curl -X POST --url http://localhost:8000/api/v1/download_pdf --output ~/Desktop/automation/reports/{md5_sample}/Static --data "hash=md5_sample" -H "Authorization:{authorization_api_key}"')
+os.system(f'curl -X POST --url http://localhost:8000/api/v1/download_pdf --output ~/Desktop/automation/reports/{md5_sample}/Static.pdf --data "hash=md5_sample" -H "Authorization:{authorization_api_key}"')
 
 # REPORT - JSON
-os.system(f'curl -X POST --url http://localhost:8000/api/v1/report_json --data "hash={md5_sample}" -H "Authorization:{$authorization_api_key}"', f'~/Automation-MobSF/reports/{md5_sample}/report.json')
+os.system(f'curl -X POST --url http://localhost:8000/api/v1/report_json --output ~/Desktop/automation/reports/{md5_sample}/report.json --data "hash={md5_sample}" -H "Authorization:{authorization_api_key}"')
 
 # DYNAMIC REPORT - PDF
-pdfkit.from_url(f'http://localhost:8000/dynamic_report/?hash={md5_sample}&package={package_sample}', f'~/Automation-MobSF/reports/{md5_sample}/Dynamic.pdf')
+pdfkit.from_url(f'http://localhost:8000/dynamic_report/?hash={md5_sample}&package={package_sample}', 'Dynamic.pdf')
